5b5f66aa46112e7894544d0d99bb50f571d6a73d
Tatenda Prince Moyo <tatendamoyo539@gmail.com>
2025-02-19 16:02:47 +0200

    change the bucket name

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "my_bucket" {
-  bucket ="ashley-my-terraform-ci-cd-bucket"
+  bucket ="tatenda-my-terraform-ci-cd-bucket"
 }
 
 resource "random_id" "sg_suffix" {
@@ -45,35 +45,33 @@ resource "aws_instance" "web" {
     Name = "Node.jsWebServer"  # This tag will be visible in the AWS Console
   }
 
-  user_data = <<-EOF
+ user_data = <<-EOF
               #!/bin/bash
               yum update -y
               yum install -y nodejs npm git
-              
+
               # Create app directory
               mkdir -p /home/ec2-user/app
               cd /home/ec2-user/app
-              
+
               # Create simple Node.js app
               echo "const http = require('http');" >> app.js
               echo "const server = http.createServer((req, res) => {" >> app.js
               echo "  res.statusCode = 200;" >> app.js
-              echo "  res.setHeader('Content-Type', 'text/plain');" >> app.js
-              echo "  res.end('Hello, Welcome to Up The Chelsea Tech!Terraform CI/CD!\n');" >> app.js
+              echo "  res.setHeader('Content-Type', 'text/html');" >> app.js
+              echo "  res.end('<h1 style=\"font-size:30px; text-align:center;\">Hello, Welcome To Up The Chels Tech!<br>From Node.js App on EC2 Deployed Using GitHub Actions and Terraform!</h1>');" >> app.js
               echo "});" >> app.js
               echo "server.listen(3000, '0.0.0.0', () => {" >> app.js
               echo "  console.log('Server running on port 3000');" >> app.js
               echo "});" >> app.js
-              
+
               # Install PM2 to keep the app running
               npm install -g pm2
               pm2 start app.js
               pm2 startup
               pm2 save
-              EOF
+EOF
 }
-
-
 output "ec2_public_ip" {
   value = aws_instance.web.public_ip
 }
